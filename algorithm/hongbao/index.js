9ed95cd8c89c6d95ccd7932d0547c4718b6ed723
hgf635381152 <635381152@qq.com>
2020-04-16 19:08:33 +0800

    实现了红包算法

---
+++ algorithm/hongbao/index.js
@@ -0,0 +1,18 @@
+//发红包的功能
+function hongbao(total,num)
+{
+    let arr=[];//声明数组arr
+    let restAmount=total;
+    let restNum=num;
+    for(let i =0;i<num-1;i++)
+    {
+        let amount = (Math.random() * ((restAmount-0.01*(restNum-1)) - 0.01) + 0.01).toFixed(2);
+        arr.push(amount)
+        restAmount -= amount;
+        restNum -= 1;
+    }
+    restAmount=restAmount.toFixed(2);
+    arr.push(restAmount)
+    return arr;
+}
+console.log(hongbao(5,40));
