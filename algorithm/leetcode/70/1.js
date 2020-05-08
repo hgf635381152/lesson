2690e23d64b133a4001d949e6f8f7f07214ab47d
hgf635381152 <635381152@qq.com>
2020-05-08 16:31:53 +0800

    爬楼梯算法的优化与思考

---
+++ algorithm/leetcode/70/1.js
@@ -0,0 +1,13 @@
+/** 
+* @author hgf
+* @date 2020-4-14
+* @func 爬楼梯算法
+* @param {@type number} n
+*/
+function climStairs(n){
+    if (n >= 40) throw new Error('内存会溢出')
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    return climStairs(n-1) + climStairs(n-2);
+}
+console.log(climStairs(120))
---
+++ algorithm/leetcode/70/3.js
@@ -0,0 +1,16 @@
+// 优化爬楼梯算法
+function f(n) {
+    const w = new Map();  // es6 新的数据类型 map
+    if (n >= 40) throw new Error('内存会溢出')
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    if (w.has(n)) {
+        return w.get(n);
+    }
+    const ret = f(n-1) +f(n-2);
+    // 存一下各层的走法数？
+    w.set(n, ret);
+    // console.log(n, '------------')
+    return ret;
+}
+console.log(f(30));
