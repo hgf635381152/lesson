2425c7845622035a19a9407a8190c6488976b877
hgf635381152 <635381152@qq.com>
2020-05-20 13:09:53 +0800

    初始react

---
+++ react/f-react/src/CommentList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function CommentList() {
+  return ( 
+    <ul>
+      <li>
+        用户名：123
+      </li>
+      <li>
+        用户名：123
+      </li>
+      <li>
+        用户名：123
+      </li>
+      <li>
+        用户名：123
+      </li>
+    </ul>
+  )
+}
+
+export default CommentList;
---
+++ react/f-react/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; // UI function JSX
+import ReactDOM from 'react-dom';
+// 入口文件
+import CommentAPP from './CommentAPP';
+
+ReactDOM.render(
+  <CommentAPP />,
+  document.getElementById('root')
+  );
