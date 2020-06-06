17bba62cde94602c37f476d82e306154937aea92
hgf635381152 <635381152@qq.com>
2020-06-06 21:16:40 +0800

    mango-muscig 完善

--- react/mango-music-chapter/src/api/recommend.js
+++ react/mango-music-chapter/src/api/recommend.js
@@ -51,7 +51,6 @@ export function getNewAlbum() {
           singer: '程小溪',
           publicTime: '2018-01-28'
         },
-        ,
         {
           id: 8,
           img: 'http://news.lyd.com.cn/pic/003/003/415/00300341501_742c978b.jpg',
@@ -68,11 +67,11 @@ export function getNewAlbum() {
         },
         {
           id: 10,
-          img: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1591448805&di=6ca51376ab651500b343e829ccb5ed83&src=http://a3.att.hudong.com/68/61/300000839764127060614318218_950.jpg',
+          img: 'http://news.lyd.com.cn/pic/003/003/415/00300341501_742c978b.jpg',
           name: '断桥',
           singer: '程小溪',
           publicTime: '2018-01-28'
-        },])
+        }])
     }, 3000)
   })
 }
