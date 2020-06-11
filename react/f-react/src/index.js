f99e4d5945042c9eafba450fdf98b77ebf33f85e
hgf635381152 <635381152@qq.com>
2020-06-11 13:53:29 +0800

    delete f-react

--- react/f-react/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'; // UI function JSX
-import ReactDOM from 'react-dom';
-// 入口文件
-import CommentAPP from './CommentAPP';
-
-ReactDOM.render(
-  <CommentAPP />,
-  document.getElementById('root')
-  );
