911535be1ceb6f532be1be5e289d3909123f6b26
hgf635381152 <635381152@qq.com>
2020-06-13 20:52:02 +0800

    lt 70

--- algorithm/leetcode/70/1.js
+++ algorithm/leetcode/70/1.js
@@ -1,13 +1,16 @@
-/** 
-* @author hgf
-* @date 2020-4-14
-* @func 爬楼梯算法
-* @param {@type number} n
-*/
-function climStairs(n){
-    if (n >= 40) throw new Error('内存会溢出')
+var climbStairs = function (n) {
     if (n == 1) return 1;
     if (n == 2) return 2;
-    return climStairs(n-1) + climStairs(n-2);
-}
-console.log(climStairs(120))
+    // 避免重复计算  节约内存  变量可以申请一个空间给不同的值使用
+    let ret = 0,
+        pre = 2,
+        prepre = 1;
+
+    for (let i = 3; i <= n; i++) {
+        ret = pre + prepre;
+        prepre = pre;
+        pre = ret;
+    }
+
+    return ret;
+};
