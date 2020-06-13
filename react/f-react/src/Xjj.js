b23e4e545a7bd7ba4c8d22b367488f7c8597f745
hgf635381152 <635381152@qq.com>
2020-06-13 17:35:21 +0800

    jsp-f-react 基础完善

--- react/f-react/src/Xjj.js
+++ react/f-react/src/Xjj.js
@@ -1,16 +1,54 @@
 import React, { Component, Fragment } from 'react';
 import XjjItem from './XjjItem';
+import Boss from './Boss';
+import axios from 'axios';
 import './style.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 class Xjj extends Component{
+  // 在某一时刻   可以自动执行的函数
+  // constructor 是 es6 的语法  构造函数
   constructor(props){
     super(props)
     this.state = {
       inputValue: '',
-      list: ['基础按摩','精油推背']
+      list: []
     }
   }
+
+  // componentWillMount() {
+  //   console.log('componentWillMount------组件将要挂载到页面的时候')
+  // }
+  // componentDidMount() {
+  //   console.log('componentDidMount------组件挂载完成的时候')
+  // }
+  // shouldComponentUpdate(){
+  //   console.log('1-shouldComponentUpdate------')
+  //   return true
+  //   // return false的话  截至到这就不会往下执行了
+  // }
+  // componentWillUpdate(){
+  //   console.log('2-componentWillUpdate-------')
+  // }
+  // componentDidUpdate() {
+  //   console.log('4-componentDidUpdate---------')
+  // }
+
+  componentDidMount(){
+    axios.get('https://www.easy-mock.com/mock/5ee489a65f62a4772f77fe95/f-react/Xjj')
+      .then((res) => {
+        console.log('axios 获取数据成功'+JSON.stringify(res))
+        this.setState({
+          list: res.data.data
+        })
+      })
+      .catch((error) => {
+        console.log('axios 获取数据失败'+error)
+      })
+  }
+
   render(){
+    // console.log('render------挂载中')
     return(
       <Fragment>
         {/* 这是一个注释 */}
@@ -26,19 +64,30 @@ class Xjj extends Component{
           <button onClick={this.addList.bind(this)}>增加服务</button>
         </div>
         <ul ref={(ul)=>{this.ul=ul}}>
-          {
-            this.state.list.map((item, index) => {
-              return (
-                  <XjjItem 
+          <TransitionGroup>
+            {
+              this.state.list.map((item, index) => {
+                return (
+                  <CSSTransition
+                    timeout={2000}
+                    classNames="boss-text"
+                    unmountOnExit
+                    appear={true}
                     key={index+item}
-                    content={item}
-                    index={index}
-                    deleteItem={this.deleteItem.bind(this)}
-                  />        
-              )
-            })
-          }
+                  >
+                    <XjjItem 
+                      // key={index+item}
+                      content={item}
+                      index={index}
+                      deleteItem={this.deleteItem.bind(this)}
+                    />  
+                  </CSSTransition>      
+                )
+              })
+            }
+          </TransitionGroup>
         </ul>
+        <Boss />
       </Fragment>
     )
   }
@@ -52,8 +101,6 @@ class Xjj extends Component{
     this.setState({
       list: [...this.state.list, this.state.inputValue],
       inputValue: ''
-    },() => {
-        console.log(this.ul.querySelectorAll('li').length)
     })
     // console.log(this.ul.querySelectorAll('li').length)
     // 异步  所以要回调
--- react/f-react/src/XjjItem.js
+++ react/f-react/src/XjjItem.js
@@ -6,10 +6,30 @@ class XjjItem extends Component {
     super();
     this.handleClick = this.handleClick.bind(this)
   }
+  // 组件第一次存在于dom中， 函数是不会被执行的
+  // 如果已经存在于dom中， 函数才会执行
+  // componentWillReceiveProps() {
+  //   console.log('child-componentWillReceiveProps-------')
+  // }
+  // // 子组件将要被删除时执行
+  // componentWillUnmount() {
+  //   console.log('child-componentWillUnmount--------')
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if(nextProps.content !== this.props.content){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
   render() { 
+    console.log('child-render-----')
     return ( 
       <li onClick={this.handleClick}>
-        {this.props.aname}为你服务-{this.props.content}
+        {this.props.aname}为你导航-{this.props.content}
       </li>
      );
   }
