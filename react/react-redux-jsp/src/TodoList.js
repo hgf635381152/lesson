c1fc5741dde8b2f7d728c243eafdbf95ddd3081a
hgf635381152 <635381152@qq.com>
2020-06-15 21:52:18 +0800

    react-redux-jsp完善

---
+++ react/react-redux-jsp/src/TodoList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd';
+import store from './store/index';
+
+
+class TodoList extends Component {
+  constructor(props){
+    super(props);
+    console.log(store.getState())
+    this.state = store.getState()
+  }
+  render() { 
+    return ( 
+      <div style={{margin:'20px'}}>
+        <div>
+          <Input 
+            placeholder={this.state.inputValue} 
+            style={{ width:'250px', marginRight:'20px'}}
+          />
+          <Button type="primary">增加</Button>
+        </div>
+        <div style={{margin:'10px', width:'300px'}}>
+          <List 
+            bordered
+            dataSource={this.state.list}
+            renderItem={item => (<List.Item>{item}</List.Item>)}
+          />
+        </div>
+      </div>
+     );
+  }
+}
+ 
+export default TodoList;
---
+++ react/react-redux-jsp/src/store/reducer.js
@@ -0,0 +1,12 @@
+const defalutState = {
+  inputValue: 'Write Something',
+  list: [
+    '早八点开晨会',
+    '早九点沟通',
+    '早十点工作'
+  ]
+}
+
+export default (state = defalutState, action) => {
+  return state
+}
