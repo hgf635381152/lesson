121f2942b9b5eb13e1ee1189ca2fe6465a8ba985
hgf635381152 <635381152@qq.com>
2020-06-20 13:16:26 +0800

    react-immutable

--- react/react-immutable/src/index.js
+++ react/react-immutable/src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+let obj = {};
+let obj1 = obj;
+let obj2 = obj1;
+
+obj1.name = 123;  // 本意是修改obj1，可是obj、obj2也受到了影响
+
+console.log(obj, obj2);
+// 可变  万恶之源
+
+// 不可变
+let newObj = {}
+let obj3 = {
+  ...newObj
+}
+obj3.name = 123;
+console.log(obj3, newObj);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
