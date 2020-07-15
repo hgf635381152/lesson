553256db9112cd7a568aa4f4a3a9114e17ee4741
hgf635381152 <635381152@qq.com>
2020-07-15 21:37:46 +0800

    react-router-jsp end

--- react/react-router-jsp-demo2/src/AppRouter.js
+++ react/react-router-jsp-demo2/src/AppRouter.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Index from './Pages/Index';
+import './index.css';
+import Video from './Pages/Video'
+import Workpalce from './Pages/Workplace'
 
 function AppRouter() {
+  let routerConfig = [
+    { path: '/', title: '博客首页', exact: true, component: Index },
+    { path: '/video', title: '视频教程', exact: false, component: Video },
+    { path: '/workplace', title: '职场技能', exact: false, component: Workpalce }
+  ]
   return(
     <Router>
       <div className="mainDiv">
         <div className="leftNav">
           <h3>一级导航</h3>
           <ul>
-            <li><Link to="/">博客首页</Link></li>
-            <li><Link to="/">视频教程</Link></li>
-            <li><Link to="/">职场技能</Link></li>
+            {
+              routerConfig.map((item, index) => {
+                return (
+                  <li key={index}><Link to={item.path}>{item.title}</Link></li>
+                )
+              })
+            }
           </ul>
         </div>
         <div className="rightMain">
-          <Route path="/" exact component={Index} />
+          {
+            routerConfig.map((item, index) => {
+              return (
+                <Route key={index} path={item.path} exact={item.exact} component={item.component} />
+              )
+            })
+          }
         </div>
       </div>
     </Router>
