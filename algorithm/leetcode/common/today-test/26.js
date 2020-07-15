de51dd023a42df16826645a82287b9dbf2433ae0
hgf635381152 <635381152@qq.com>
2020-07-15 09:38:15 +0800

    leetcode 26

---
+++ algorithm/leetcode/common/today-test/26.js
@@ -0,0 +1,9 @@
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i + 1] == nums[i]) {
+      nums.splice(i + 1, 1);
+      i--
+    }
+  }
+  return nums.length
+};
